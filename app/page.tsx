fc53e74ae2adb325de69d7d4d960e7b0b56491b5
Martin Lowe <martinlowe673@gmail.com>
2024-09-15 19:42:54 +0100

    form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Card } from "@/components/ui/card";
 
 
 export default function Home() {
   return (
     <section className="min-h-screen w-screen flex flex-col items-center justify-center px-5">
-     <h1>Contact us</h1>
+     <h1 className="text-4xl font-bold">Contact us</h1>
+     <Card className="max-w-[500px] w-full"></Card>
     </section>
   );
 }
