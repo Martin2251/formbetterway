9bdd4690ee24dc225fa264a56197f9956b3c487a
Martin Lowe <martinlowe673@gmail.com>
2024-09-16 19:22:46 +0100

    success route

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -4,9 +4,10 @@ export default function SuccessRoute(){
     return(
         <section className="h-screen w-screen flex flex-col items-center justify-center">
             <div className="flex flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
-                <div>
+                <div className="flex size-20 items-center justify-center rounded-full bg-green-400/10">
                     <CheckIcon className="size-10 text-green-500"  />
                 </div>
+                <h2 className="mt-6 text-xl font-semibold">Success we got your message</h2>
             </div>
         </section>
     )
