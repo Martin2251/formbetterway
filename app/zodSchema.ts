531843d3cc2a2d42a2053c2b86d5a89af640ee3b
Martin Lowe <martinlowe673@gmail.com>
2024-09-17 07:38:25 +0100

    zod schema

--- app/zodSchema.ts
+++ app/zodSchema.ts
@@ -3,5 +3,7 @@ import {z} from 'zod'
 
 export const submissionSchema = z.object([
     name: z.string().min(2),
-    email:z.string()
+    email:z.string().email(),
+    message:z.string().min(2).max(1500),
+    image:z.instanceof(File).optional(),
 ])
