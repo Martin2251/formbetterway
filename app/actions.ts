05abb516e65b95dab2d9a3eebdbbe1e05a7bc836
Martin Lowe <martinlowe673@gmail.com>
2024-09-17 07:41:24 +0100

    created submission

--- app/actions.ts
+++ app/actions.ts
@@ -1,10 +1,21 @@
 "use server"
 
+import { parseWithZod } from "@conform-to/zod"
 import { redirect } from "next/navigation"
+import { submissionSchema } from "./zodSchema"
 
 // store our server actions 
 
 export async function TalkToSalesAction(formData:FormData){
+
+    const submission = parseWithZod(formData,{
+        schema:submissionSchema,
+    })
+
+    if(submission.status !== "success"){
+        return submission.reply();
+    }
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const response = await fetch(process.env.TALK_TO_SALES_URL!,{
         method: 'POST',
